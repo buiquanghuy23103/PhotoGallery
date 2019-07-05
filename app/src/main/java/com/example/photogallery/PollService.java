32f2bed21f1ed984a173a40e5a514037ae26fc32
huybui <huythdnbkltv@gmail.com>
2019-07-05 21:40:39 +0300

    Use JobService instead of AlarmManager

--- app/src/main/java/com/example/photogallery/PollService.java
+++ app/src/main/java/com/example/photogallery/PollService.java
@@ -4,9 +4,15 @@ import android.app.AlarmManager;
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
+import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -17,55 +23,80 @@ import androidx.core.app.NotificationManagerCompat;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class PollService extends IntentService {
+public class PollService extends JobService {
     public static final String SHOW_NOTIFICATION_ACTION = "photoGallery.PollService.SHOW_NOTIFICATION";
     private static final String TAG = "PollService";
     private static final long SERVICE_INTERVAL = TimeUnit.MINUTES.toMillis(1);
     private static final int POLL_SERVICE_REQUEST_CODE = 0;
     private static final int NOTIFICATION_ID = 0;
+    private static final int JOB_ID = 1234;
+
+    private PollTask mPollTask;
 
     public PollService() {
-        super(TAG);
     }
 
     public static Intent newIntent(Context context) {
         return new Intent(context, PollService.class);
     }
 
-    public static boolean isServiceOn(Context context){
-        Intent intent = PollService.newIntent(context);
-        PendingIntent pendingIntent = PendingIntent.getService(
-                context,
-                POLL_SERVICE_REQUEST_CODE,
-                intent,
-                PendingIntent.FLAG_NO_CREATE);
-        return pendingIntent != null;
-    }
-
-    public static void setServiceOn(Context context, boolean isOn){
-        Intent intent = new Intent(context, PollService.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context, POLL_SERVICE_REQUEST_CODE,
-                intent, 0);
-        AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-
-        if (isOn){
-            alarmManager.setRepeating(AlarmManager.ELAPSED_REALTIME,
-                    SystemClock.elapsedRealtime(),
-                    SERVICE_INTERVAL,
-                    pendingIntent);
-            Log.i(TAG, "Service is on");
-        } else {
-            alarmManager.cancel(pendingIntent);
-            pendingIntent.cancel();
-        }
-
-        QueryPreference.setServiceStatus(context, true);
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        mPollTask = new PollTask();
+        mPollTask.execute(params);
+        return true;
     }
 
     @Override
-    protected void onHandleIntent(@Nullable Intent intent) {
-        if (isNetworkConnected()){
+    public boolean onStopJob(JobParameters params) {
+        if (mPollTask != null){
+            mPollTask.cancel(true);
+        }
+        return true;
+    }
+
+    private class PollTask extends AsyncTask<JobParameters, Void, Void>{
+        @Override
+        protected Void doInBackground(JobParameters... jobParameters) {
             pollPhotos();
+            jobFinished(jobParameters[0], false);
+            return null;
+        }
+    }
+
+    public static boolean isServiceOn(Context context){
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(JOB_SCHEDULER_SERVICE);
+        for (JobInfo jobInfo:
+             jobScheduler.getAllPendingJobs()) {
+            if (jobInfo.getId() == JOB_ID){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void setServiceOn(Context context, boolean isOn){
+        setServiceSchedule(context, isOn);
+        Log.i(TAG, "Service is activated and put into schedule");
+        QueryPreference.setServiceStatus(context, isOn);
+    }
+
+    private static void setServiceSchedule(Context context, boolean isOn) {
+        ComponentName componentName = new ComponentName(context, PollService.class);
+        JobInfo jobInfo = new  JobInfo.Builder(JOB_ID, componentName)
+                .setPeriodic(SERVICE_INTERVAL) // 1 minute
+                .setPersisted(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .build();
+
+        JobScheduler jobScheduler = (JobScheduler) context.getSystemService(JOB_SCHEDULER_SERVICE);
+
+        if (isOn){
+            jobScheduler.schedule(jobInfo);
+            Log.i(TAG, "Service scheduled");
+        } else {
+            jobScheduler.cancel(JOB_ID);
+            Log.i(TAG, "Service is cancelled");
         }
     }
 
